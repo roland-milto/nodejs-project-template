12cf25ae6006c6c15df83e5d9fdcc67ce708ed1c
Roland Milto <34012369+roland-milto@users.noreply.github.com>
2026-04-07 23:14:43 +0200

    Initial commit.

---
+++ prettier.config.js
@@ -0,0 +1,18 @@
+export default {
+	"arrowParens": "always",
+	"bracketSameLine": true,
+	"bracketSpacing": true,
+	"embeddedLanguageFormatting": "auto",
+	"endOfLine": "lf",
+	"htmlWhitespaceSensitivity": "css",
+	"jsxSingleQuote": false,
+	"quoteProps": "as-needed",						// Or "consistent"
+	"printWidth": 100,
+	"proseWrap": "always",
+	"semi": true,
+	"singleAttributePerLine": false,
+	"singleQuote": true,
+	"trailingComma": "es5",
+	"useTabs": false,
+		"tabWidth": 2
+};
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+// TODO: Create a perfect app
+// TODO(2026-04-30): Till this date
+// FIXME: App is not running
+// FIXME(@roland): With name
+// NOTE: You can do it! Because you are great! :)
